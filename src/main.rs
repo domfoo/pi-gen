2d9b3be5be9ec776960c82a6ec4df536949d7a20
Dominik Bartsch <dominik.bartsch@gmx.net>
2021-04-12 21:48:27 +0200

    add program logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rand::{thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    // generate a random number in range [0;1)
+    let rand = || thread_rng().gen();
+
+    let mut n1 = 0;
+    let n2 = 10000000;
+    
+    for _i in 0..n2 {
+        let x: f64 = rand();
+        let y: f64 = rand();
+
+        // check if the point is inside the circle (only works with coordinates in range [0;1))
+        if x * x + y * y < 1.0 {
+            n1 += 1;
+        }
+    }
+
+    // PI is 4 times the ratio of the points in the circle and all the points
+    let pi = 4.0 * n1 as f64 / n2 as f64;
+
+    println!("PI is approximately {}", pi);
 }
